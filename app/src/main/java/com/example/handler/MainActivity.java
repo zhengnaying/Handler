9959bb8d3bdd0a5d6d95244a49bca15ca4db572c
zhengnaying <55484284+zhengnaying@users.noreply.github.com>
2020-11-09 19:30:05 +0800

    1

--- app/src/main/java/com/example/handler/MainActivity.java
+++ app/src/main/java/com/example/handler/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+ Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = (Button)findViewById(R.id.b1);
+
     }
 }
