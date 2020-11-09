b71ce7829c19c03bd605fa8c576a482d8f4df142
zhengnaying <55484284+zhengnaying@users.noreply.github.com>
2020-11-09 19:38:31 +0800

    1

--- app/src/main/java/com/example/handler/MainActivity.java
+++ app/src/main/java/com/example/handler/MainActivity.java
@@ -3,15 +3,54 @@ package com.example.handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
  Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         b1 = (Button)findViewById(R.id.b1);
+        final TextView t1 =findViewById(R.id.text);
+        final Handler handler = new Handler(){
+            public void handMessage(Message msg){
+                t1.setText(msg.arg1+"");
+            }
+        };
+        final Runnable myWorker = new Runnable() {
+            @Override
+            public void run() {
+                int progress = 0;
+                while (progress <= 100){
+                    Message msg = new Message();
+                    msg.arg1 = progress;
+                    handler.sendMessage(msg);
+                    progress+=10;
+                    try{
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Message msg = handler.obtainMessage();
+                msg.arg1 = -1;
+                handler.sendMessage(msg);
+            }
+        };
+        b1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Thread workThread = new Thread(null,myWorker,"WorkThread");
+                workThread.start();
+            }
+        });
 
     }
 }
